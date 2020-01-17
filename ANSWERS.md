cd548f8d92370bb1e8fb59eabc2e46dad917968f
Jessica Wolff <jleahwolff@gmail.com>
2020-01-17 13:06:16 -0600

    MVP almost

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,4 +1,10 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
+    Class Components are very common in the workplace, and have been around longer, therefore it's good to know of class compoentns. 
 - [ ] Name three lifecycle methods and their purposes.
+    render() = returns the this.state and this.props, and is a required method of class components.
+    constructor() = initialized local state, and binds event handlers (if used)
+    componentDidMount() = invoked immediately once component is mounted. Sets up state, which will rerender.  Cannot forget to unmount. 
 - [ ] What is the purpose of a custom hook?
+    Dry code, reusability, clean
 - [ ] Why is it important to test our apps?
+    Turning in code that hasn't been tested is a bad protocol.  Everything should be ensured running before turning it in to test for bugs, or errors. 
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from '../src/components/Navbar';
+import waitForExpect from 'wait-for-expect';
+import '@testing-library/jest-dom/extend-expect';
+
+import { render, getByText } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// test('dark mode is firing', () => {
+//   const { getByTestId } = render(<App/>)
+//   const namesAppear = getTestById("athlete")
+//   expect(namesAppear.textContent).toBe()
+// })
+
+// test("dark mode is firing" , () => {
+//   const testDarkMode = 'Dark Mode is firing'
+//   render(<Navbar>{testDarkMode}</Navbar>)
+//   expect(screen.getByText(testDarkMode)).toBeNull()
+// })
+
+test("rendering properly", () => {
+  render( <App />)
+})
